14cd0aa8f2f7e66a0ccf18de6ddddde838db3967
arbishghori <128428077+arbishghori@users.noreply.github.com>
2023-08-08 04:37:07 -0700

    Add files via upload

---
+++ ar git 5/script.js
@@ -0,0 +1,9 @@
+ var month = ["january","febrary","march","april","may","june"];
+
+ var month = prompt("Enter a month");
+ var charsInMonth = month.length;
+if (charsInMonth > 3){
+  monthAbbrev = month.slice(0, 3);{
+    alert("its my birthday month");
+  }
+ }
